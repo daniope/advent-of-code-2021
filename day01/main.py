effb344d738ca1f8f894c5e570b04f41cb58ff78
daniope <daniela.iope@hotmail.com>
2022-05-08 20:02:21 -0300

    feat: impls solution for day 01

--- day01/main.py
+++ day01/main.py
@@ -1,6 +1,23 @@
-def main():
-    pass
+import numpy as np
 
+
+def get_number_of_increases(ds):
+    return len([
+        y - x for x, y in zip(ds, ds[1:])
+        if (y - x) > 0
+        ])
+
+def main():
+    ds = np.loadtxt('input.txt')
+    n = get_number_of_increases(ds)
+    print('Part 1')
+    print('Number of increases: {:d}\n'.format(n))
+
+    window = np.ones(3)
+    ds_c = np.convolve(ds, window, mode='valid')
+    n = get_number_of_increases(ds_c)
+    print('Part 2')
+    print('Number of increases: {:d}'.format(n))
 
 if __name__ == '__main__':
     main()
